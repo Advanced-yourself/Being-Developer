d6204212a8e8dd4dceeef10eca9e2f699fcaaf41
anurag <anuragbadwahe@gmail.com>
2024-01-10 22:36:49 +0530

    i

--- src/component/LoginPage/LoginPage.jsx
+++ src/component/LoginPage/LoginPage.jsx
@@ -24,7 +24,7 @@ const LoginPage = () => {
         // const message = res.data.message;
       
         if (token) {
-          localStorage.setItem("loginToken", token);
+          localStorage.setItem("token", token);
           window.alert("Login Successful");
         } else {
           navigate("/signup");
